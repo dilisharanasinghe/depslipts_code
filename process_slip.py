b18a553f26ec5d07f606ceb7187f6b5492d71b2c
dilisharanasinghe <dilisharanasinghe@gmail.com>
2021-02-26 14:51:43 +0530

    git ignore

--- process_slip.py
+++ process_slip.py
@@ -37,16 +37,16 @@ if __name__ == '__main__':
     pass
     special_check = None  # '1599152317.jpg'
     with open('total_results.txt', 'w') as f:
-        for root, dirs, files in os.walk("../../Data", topdown=False):
+        for root, dirs, files in os.walk("test_data", topdown=False):
             for name in files:
                 if special_check is not None:
                     if special_check in name:
                         path = os.path.join(root, name)
                         s = 'File path {0}\n'.format(path)
                         print(s)
-                        ProcessSlip.do_the_thing(path)
+                        print(ProcessSlip.do_the_thing(path))
                 else:
                     path = os.path.join(root, name)
                     s = 'File path {0}\n'.format(path)
                     print(s)
-                    ProcessSlip.do_the_thing(path)
+                    print(ProcessSlip.do_the_thing(path))
